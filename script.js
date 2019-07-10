76468e22a3f4dd0fd466e917d9e974854ee1c6da
Krzysztof Branecki <krzysztof@branecki.net>
2019-07-10 12:54:56 +0200

    Self

--- script.js
+++ script.js
@@ -69,3 +69,22 @@ onePlusOne.printInfo();
 onePlusOne.printIMEI();
 samsungS6.compare(iPhone6S);
 iPhone6S.compare(onePlusOne);
+
+function Button(text) {
+  this.text = text || "Hello";
+}
+Button.prototype = {
+  create: function() {
+    var self = this;
+    this.element = document.createElement("button");
+    this.element.innerText = this.text;
+
+    this.element.addEventListener("click", function() {
+      alert(self.text);
+    });
+    document.body.appendChild(this.element);
+  }
+};
+
+var btn1 = new Button("Hello!");
+btn1.create();
